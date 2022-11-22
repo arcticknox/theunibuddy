fa1edc38522251de9972432b601b6aa979551de0
Shivam Thabe <113085710+thabes@users.noreply.github.com>
2022-11-22 13:02:39 -0500

    added morgan middleware for error logging (#6)

--- api/app.js
+++ api/app.js
@@ -4,12 +4,17 @@ import routes from './routes/index.js';
 import passport from 'passport';
 import jwtStrategy from './utils/jwtStrategy.js';
 import errorHandlerMiddleware from './middlewares/error.middlware.js';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); // Parse json
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
+// create "middleware"
+app.use(morgan('combined'));
 // JWT auth strategy
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
