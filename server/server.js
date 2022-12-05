ed93e757cc71a7fe14cb7142b2120cc1345cf6bb
AkshayParab95 <113443337+AkshayParab95@users.noreply.github.com>
2022-12-04 22:22:26 -0500

    Added gracefull shutdown for Docker stop events and otherwise (#36)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import config from './config/index.js';
 import mongoose from 'mongoose';
+import stoppable from 'stoppable';
 import app from './api/app.js';
 import centralErrorHandler from './api/utils/centralErrorHandler.js';
 
@@ -14,10 +15,12 @@ const initMongoConn = () => {
       });
 };
 
-app.listen(config.app.port, () => {
-  initMongoConn();
-  console.log('Server listening on port', config.app.port);
-});
+const server = stoppable(
+    app.listen(config.app.port, () => {
+      initMongoConn();
+      console.log('Server listening on port', config.app.port);
+    }),
+);
 
 // Centralized error handler
 app.use((err, req, res, next) => {
@@ -30,4 +33,29 @@ process.on('uncaughtException', (error) => {
 
 process.on('unhandledRejection', (reason) => {
   centralErrorHandler(reason);
+});
+
+const shutdown = () => {
+  server.close((err) => {
+    if (err) {
+      console.error('Error detected during shutdown', err);
+      process.exitCode = 1;
+    }
+    console.log('MongoDB gracefully disconnected');
+    mongoose.connection.close();
+    process.exit();
+  });
+};
+
+
+// Quit on CTRL - C when running Docker in Terminal
+process.on('SIGINT', ()=>{
+  console.log('Received SIGINT, gracefully shutting down');
+  shutdown();
+});
+
+// Quit on docker stop command
+process.on('SIGTERM', () => {
+  console.info('Received SIGTERM, gracefully shutting down');
+  shutdown();
 });
